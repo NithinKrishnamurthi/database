e203f1854ec60416e6e52c9544f680aa1b3c81d2
Nithin Krishnamurthi <nithin.krishnamurthi401@gmail.com>
2017-01-25 13:57:45 -0800

    Finished Project 0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,68 @@
+
 public class NBody {
+	public static void main(String[]args){
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Planet[] planets = readPlanets(filename);
+		StdDraw.setScale(-radius, radius);
+		StdDraw.clear();
+		StdDraw.picture(0,0,"images/starfield.jpg");
+		for(int i = 0;i<planets.length;i++){
+			planets[i].draw();
+		}
+		double t = 0;
+		while(t<T){
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
+			for(int i = 0;i<planets.length;i++){
+				for(int j = 0;j<planets.length;j++){
+					if(i!=j){
+						xForces[i] += planets[i].calcForceExertedByX(planets[j]);
+						yForces[i] += planets[i].calcForceExertedByY(planets[j]);
+					}
+				}
+			}
+			for(int i = 0;i<planets.length;i++){
+				planets[i].update(dt,xForces[i],yForces[i]);					
+			}
+			
+			StdDraw.picture(0,0,"images/starfield.jpg");
+			for(int i = 0;i<planets.length;i++){
+				planets[i].draw();
+			}
+			StdDraw.show(10);
+			t = t+dt;
+		}
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < planets.length; i++) {
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		   		planets[i].xxPos, planets[i].yyPos, planets[i].xxVel, planets[i].yyVel, planets[i].mass, planets[i].imgFileName);	
+		}
+	}
+	public static double readRadius(String fileName){
+		In file = new In(fileName);
+		file.readDouble();
+		return file.readDouble();	
+	}
+	public static Planet[] readPlanets(String fileName){
+		In file = new In(fileName);
+		int numPlanets = file.readInt();
+		file.readDouble();
+		Planet[] planets = new Planet[numPlanets];
+		for (int i = 0;i<numPlanets;i++){
+			double xP = file.readDouble();
+			double yP = file.readDouble();
+			double xV = file.readDouble();
+			double yV = file.readDouble();
+			double m = file.readDouble();
+			String img = file.readString();
+			Planet p = new Planet(xP,yP,xV,yV,m,img);
+			planets[i] = p;
+		}
+		return planets;
+	}
 
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,53 @@
 public class Planet {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+	
+	public Planet(double xP,double yP, double xV, double yV, double m, String img){
+		this.xxPos = xP;
+		this.yyPos = yP;
+		this.xxVel = xV;
+		this.yyVel = yV;
+		this.mass = m;
+		this.imgFileName = img; 
+	}
+	
+	public Planet(Planet p){
+		this.xxPos = p.xxPos;
+		this.yyPos = p.yyPos;
+		this.xxVel = p.xxVel;
+		this.yyVel = p.yyVel;
+		this.mass = p.mass;
+		this.imgFileName = p.imgFileName; 
+	}
+	public double calcDistance(Planet p){
+		double dx = p.xxPos - this.xxPos;
+		double dy = p.yyPos - this.yyPos;
+		return java.lang.Math.pow(java.lang.Math.pow(dx,2) + java.lang.Math.pow(dy,2),0.5);
+ 	}
+	public double calcForceExertedBy(Planet p){
+		return this.mass*p.mass*6.67*java.lang.Math.pow(10,-11)/java.lang.Math.pow(this.calcDistance(p),2);
+	}
+	public double calcForceExertedByX(Planet p){
+		double dx = p.xxPos - this.xxPos;
+		return this.calcForceExertedBy(p)*(dx)/this.calcDistance(p);
+	}
+	public double calcForceExertedByY(Planet p){
+		double dy = p.yyPos - this.yyPos;
+		return this.calcForceExertedBy(p)*(dy)/this.calcDistance(p);
+	}
+	public void update(double dt,double fX,double fY){
+		double ax = fX/this.mass;
+		double ay = fY/this.mass;
+		this.xxVel = this.xxVel + ax*dt;
+		this.yyVel = this.yyVel + ay*dt;
+		this.xxPos = this.xxVel*dt + this.xxPos;
+		this.yyPos = this.yyVel*dt + this.yyPos;
+	}
+	public void draw(){
+		StdDraw.picture(this.xxPos,this.yyPos,"images/" + this.imgFileName);
+	}
 }
